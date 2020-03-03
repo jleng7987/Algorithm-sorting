60b4f2cafbc9cbbb57e484fa875f8ff2344e7f1d
jinliang <jinliang@sia.cn>
2020-03-03 16:37:37 +0800

    2020年3月3日，不用加号求和

---
+++ D3.3-不用加号求和/不用加号求和.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 16:34:26 2020
+
+@author: talen
+"""
+
+class Solution:
+    def Add(self, num1, num2):
+        return sum([num1,num2])
+        # write code here
